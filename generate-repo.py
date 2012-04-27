7ffa0c1e82d95cfb270ae2ba76939d2258b9f1e1
Mike McCune <mmccune@redhat.com>
2012-04-27 12:12:37 -0700

    add errata too

--- generate-repo.py
+++ generate-repo.py
@@ -10,28 +10,63 @@ if __name__ == '__main__':
  
     parser = OptionParser("usage: %prog [options]")
     parser.add_option('--numpackages',  dest='numpackages',  type="int", default=5)
+    parser.add_option('--multiversion',  dest='multiversion',  action="store_true", default=False)
+    parser.add_option('--outputdir',  dest='outputdir',  type="str", default='/var/tmp/generated-repo')
     (options, args) = parser.parse_args(sys.argv[1:])
 
-    os.system("rm -rf /var/tmp/generated-repo")
-    os.system("mkdir /var/tmp/generated-repo")
+    outputdir = options.outputdir
+    os.system("rm -rf %s" % outputdir)
+    os.system("mkdir %s" % outputdir)
 
     file = open('/usr/share/dict/words')
     lines = file.readlines()
     version_numbers = range(10)
     packages = options.numpackages
+    all_errata = ""
     for i in range(packages):
         name = random.choice(lines).rstrip()
-        version = "%s.%s.%s" % (str(random.randint(0,10)),
-                              str(random.randint(0,10)),
-                              str(random.randint(0,10)))
+        first_rev = random.randint(0,10)
+        middle_rev = random.randint(0,10)
+        last_rev = random.randint(0,10)
+        version = "%s.%s.%s" % (str(first_rev),
+                              str(middle_rev),
+                              str(last_rev))
         size = str(random.randint(0,1000))
         # print "./generate-package.bash %s %s %s" % (name, version, size)
         os.system("./generate-package.bash %s %s %s" % (name, version, size))
+        if (options.multiversion):
+            
+            et = open('./errata-template.xml')
+            errata_template = et.read()
+            # Generate 0-3 newer versions of the package
+            for j in range(random.randint(0,3)):
+                last_rev = last_rev + 1
+                version = "%s.%s.%s" % (str(first_rev),
+                                      str(middle_rev),
+                                      str(last_rev))
+                #print "    ./generate-package.bash %s %s %s" % (name, version, size)
+                os.system("./generate-package.bash %s %s %s" % (name, version, size))
+                # Generate some errata
+                errata = errata_template
+                errata = errata.replace("%%ERRATAID%%",('RHEA-2012:000%s' % i))
+                errata = errata.replace("%%REL%%", str(last_rev))                
+                errata = errata.replace("%%NAME%%", name)
+                errata = errata.replace("%%VER%%", version)
+                all_errata += errata
+                
+    #bad string concats but I'm lazy                
+    all_errata = "<?xml version=\"1.0\"?>\n<updates>" + all_errata 
+    all_errata = all_errata + "</updates>\n"
+    errata_xml = open('%s/updateinfo.xml' % outputdir, 'w')
+    errata_xml.write(all_errata)
 
-    os.system("mv ~/rpmbuild/RPMS/noarch/*elfake* /var/tmp/generated-repo")
-    os.system("createrepo /var/tmp/generated-repo")
+    
+
+    os.system("mv ~/rpmbuild/RPMS/noarch/*elfake* %s" % outputdir)
+    os.system("createrepo %s" % outputdir)
+    os.system("modifyrepo %s/updateinfo.xml %s/repodata/" % (outputdir,outputdir))
     print "\n\n\n"
     print "==========================================================="
-    print "Your new fake repo is available at: /var/tmp/generated-repo\n"
+    print "Your new fake repo is available at: %outputdir\n" % outputdir
     
     
